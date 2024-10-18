8c5d4318aca0586afbc51cfac5e1d0e7cd86e075
Alexandre <alexandre.nichiojr@sptech.school>
2024-10-18 15:52:38 -0300

    exercícioPronto

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,101 @@
 package school.sptech;
 
+import school.sptech.exception.ArgumentoInvalidoException;
+import school.sptech.exception.LivroNaoEncontradoException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Biblioteca {
 
+    private String nome;
+    private List<Livro> livros = new ArrayList<>();
+
+    public Biblioteca(String nome,Livro livros) {
+        this.nome = nome;
+    }
+    public Biblioteca(String nome ) {
+
+    }
+
+    public void adicionarLivro(Livro livro){
+        if (livro == null || livro.getAutor() == null ||
+                livro.getAutor().trim().isEmpty() || livro.getTitulo() == null||
+        livro.getTitulo().trim().isEmpty() || livro.getDataPublicacao() == null ){
+            throw new ArgumentoInvalidoException("Valor invalido");
+        }
+
+        this.livros.add(livro);
+    }
+
+    public void removerLivroPorTitulo(String titulo) {
+        if (titulo == null || titulo.trim().isEmpty()) {
+            throw new ArgumentoInvalidoException("Título inválido");
+        }
+
+        boolean removido = livros.removeIf(livro -> livro.getTitulo().equalsIgnoreCase(titulo));
+
+        if (!removido) {
+            throw new LivroNaoEncontradoException("Livro não foi encontrado");
+        }
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo){
+        if (titulo ==null || titulo.trim().isEmpty()){
+            throw  new ArgumentoInvalidoException("Livro não foi encontrado");
+        }
+
+        return livros.stream().filter(livro -> livro.getTitulo()
+                .equalsIgnoreCase(titulo))
+                .findFirst()
+                .orElseThrow(() -> new LivroNaoEncontradoException("Livro não foi encotrado"));
+
+
+
+    }
+
+    public Integer contarLivros(){
+        return (int) livros.stream().count();
+    }
+
+    public List<Livro> obterLivrosAteAno(Integer ano) {
+        if (ano == null) {
+            throw new IllegalArgumentException("Ano não pode ser nulo");
+        }
+
+        LocalDate dataReferencia = LocalDate.of(ano, 12, 31);
+
+        return this.livros.stream()
+                .filter(livro -> livro.getDataPublicacao().isBefore(dataReferencia) ||
+                        livro.getDataPublicacao().isEqual(dataReferencia))
+                .collect(Collectors.toList());
+    }
+
+    public List<Livro> retornarTopCincoLivros(){
+        return this.livros.stream().sorted(Comparator.comparingDouble(Livro::calcularMediaAvaliacoes)
+                .reversed())
+                .limit(5)
+                .collect(Collectors.toList());
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- src/main/java/school/sptech/Livro.java
+++ src/main/java/school/sptech/Livro.java
@@ -1,5 +1,77 @@
 package school.sptech;
 
+import school.sptech.exception.ArgumentoInvalidoException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Livro {
 
+    private String titulo;
+    private String autor;
+    private LocalDate dataPublicacao;
+    private List<Avaliacao> avaliacoes;
+
+    public Livro(String titulo, String autor, LocalDate dataPublicacao,List<Avaliacao> avaliacoes) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataPublicacao = dataPublicacao;
+        this.avaliacoes = new ArrayList<>();
+
+    }
+
+
+
+    public void adicionarAvaliacao(String descricao, Double qtdEstrelas){
+            if (descricao == null|| descricao.trim().isEmpty()|| qtdEstrelas ==null || qtdEstrelas < 0  || qtdEstrelas > 5 ){
+            throw new ArgumentoInvalidoException("Descrição está nula ou quantidade de estrelas está fora do intervalo permitido (0 a 5).");
+        }
+        this.avaliacoes.add(new Avaliacao(descricao,qtdEstrelas));
+    }
+    public Double calcularMediaAvaliacoes(){
+        if (avaliacoes == null || avaliacoes.isEmpty()){
+            return 0.0;
+        }
+        return avaliacoes.stream().mapToDouble(Avaliacao::getQtdEstrelas).average().getAsDouble();
+    }
+
+
+    @Override public String toString(){
+        return "";
+    }
+
+
+    public List<Avaliacao> getAvaliacoes() {
+        return avaliacoes;
+    }
+
+    public void setAvaliacoes(List<Avaliacao> avaliacoes) {
+        this.avaliacoes = avaliacoes;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
 }
